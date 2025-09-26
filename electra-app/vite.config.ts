a8e1c02927d88d10e89434e205d64761824a5816
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-09-26 20:35:40 +0200

    por added

--- electra-app/vite.config.ts
+++ electra-app/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // fuerza a Vite a escuchar en localhost
+    port: 5173          // puerto que ya estás usando
+  }
 })
